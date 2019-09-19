1599985e8af5e057dff4dbf024e747ae82093eee
linzebin <zebinlin2018@163.com>
2019-09-19 20:14:16 +0800

    update leetcode 104 leetcode 226

---
+++ src/main/java/top100/easy/二叉树的最大深度/Solution.java
@@ -0,0 +1,22 @@
+package top100.easy.二叉树的最大深度;
+
+import top100.model.TreeNode;
+
+/**
+ * @author LinZebin
+ * @date 2019-09-19 20:10
+ */
+public class Solution {
+    // 方法定义：返回一棵二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        // 出口
+        if(root == null){
+            return 0;
+        }
+        // divide
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        // conque
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ src/main/java/top100/easy/翻转二叉树/Solution.java
@@ -0,0 +1,20 @@
+package top100.easy.翻转二叉树;
+
+import top100.model.TreeNode;
+
+/**
+ * @author LinZebin
+ * @date 2019-09-19 20:04
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
